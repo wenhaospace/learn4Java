644d4dee9482928010bdb55943a40dae773531d6
wenhao <1925005015@qq.com>
2025-11-27 22:59:12 +0800

    测试接口

---
+++ monolithicapps/experiment/src/main/java/com/wendell/experiment/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.wendell.experiment.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("test")
+public class TestController {
+
+    @GetMapping("/query1")
+    public String requestMethodName(@RequestParam String name) {
+        return "Hello " + name;
+    }
+    
+    
+
+}
