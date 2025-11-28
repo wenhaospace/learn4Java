2a98123de5a8c5ea8ced9cd9b6654d9152839943
wenhao <1925005015@qq.com>
2025-11-28 11:17:21 +0800

    移除Controller上的跨域通配符

--- monolithicapps/experiment/src/main/java/com/wendell/experiment/controller/TestController.java
+++ monolithicapps/experiment/src/main/java/com/wendell/experiment/controller/TestController.java
@@ -1,6 +1,5 @@
 package com.wendell.experiment.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
-@CrossOrigin("*")
 @RequestMapping("test")
 public class TestController {
 
