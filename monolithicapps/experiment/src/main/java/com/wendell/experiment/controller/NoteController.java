a4364d78ed54b6a3061ab9b6254ecb10790a6584
wenhao <1925005015@qq.com>
2025-12-26 16:12:24 +0800

    增加跨域和通用Java程序启动脚本

--- monolithicapps/experiment/src/main/java/com/wendell/experiment/controller/NoteController.java
+++ monolithicapps/experiment/src/main/java/com/wendell/experiment/controller/NoteController.java
@@ -8,6 +8,7 @@ import com.wendell.experiment.service.NoteService;
 
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/notes")
+@CrossOrigin("*")
 public class NoteController {
 
     @Resource
